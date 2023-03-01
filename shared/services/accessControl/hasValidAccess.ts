16f08f251df98acc5e419829782f2d814cdc633c
safwanmasarik <safwanmasarik@gmail.com>
2023-03-02 00:09:40 +0800

    bug fix

--- shared/services/accessControl/hasValidAccess.ts
+++ shared/services/accessControl/hasValidAccess.ts
@@ -3,6 +3,7 @@ import { ValidAccess } from "../../models/accessControl/ValidAccess";
 import * as data from '../../data/handler/accessControl/getUser';
 import { JsonConvert } from "json2typescript";
 import { DbUser } from "../../models/accessControl/DbUser";
+import mock_data_getUser from "../../../az_http_ship/__tests__/mock_data_getUser";
 
 let jsonConvert: JsonConvert = new JsonConvert();
 
@@ -13,7 +14,18 @@ export async function hasValidAccess(req: HttpRequest): Promise<ValidAccess> {
          *  The response will contain user permission and features.
          *  For the boilerplate we'll mock all user request as coming from Super Admin. 
          */
-        const queryData = await data.getUser("Super Admin");
+
+        /** Mocking db response.
+        *   When actual database connection is available,
+        *   then remove this mocking condition and "MockDbResponse" config.
+        */
+        let queryData: object;
+        if (process.env["MockDbResponse"] == "yes") {
+            queryData = mock_data_getUser
+        } else {
+            queryData = await data.getUser("Super Admin");
+        };
+
         let modelledDbData = jsonConvert.deserializeObject(queryData, DbUser);
         let userHasValidAccess = true;
 
