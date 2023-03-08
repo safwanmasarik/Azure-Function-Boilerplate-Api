6e8c23bd7831c1fc28fd1395be777bc0387e08ed
safwanmasarik <safwanmasarik@gmail.com>
2023-03-09 00:30:50 +0800

    Fix driver issue for SQL Server 2022. Driver defaulted to SQL Server Native Client 11.0 which is not supported.

--- shared/helpers/mssql/index.ts
+++ shared/helpers/mssql/index.ts
@@ -4,19 +4,26 @@ export function initialiseConnection() {
     let connectionString = process.env["SQLConnectionString"];
 
     const config = {
-        driver: "SQL Server",
         user: process.env["SQLUsername"],
         password: process.env["SQLPassword"],
         server: process.env["SQLServer"],
         database: process.env["SQLDatabase"],
         options: {
             encrypt: false, // for azure set to true
-            trustedConnection: true, // Use Windows authentication
+            trustedConnection: true, // Use Windows authentication (local)
             trustServerCertificate: false // change to true for local dev / self-signed certs
+        },
+        beforeConnect: function (bcConfig) {
+            // Modify the connection options here
+            /** The Driver SQL Server Native Client has been removed from SQL Server 2022. 
+             *  Source https://learn.microsoft.com/en-us/sql/relational-databases/native-client/applications/installing-sql-server-native-client?view=sql-server-ver16  
+             *  ODBC Driver 17 for SQL Server is tested working well with SQL Server 2019 & 2022 */
+            bcConfig.conn_str = bcConfig.conn_str.replace("SQL Server Native Client 11.0", "ODBC Driver 17 for SQL Server");
         }
     };
 
-    return new sql.ConnectionPool(config)
+    let pool = new sql.ConnectionPool(config);
+    return pool;
 }
 
 export { sql };
