85508c6147abc2b9cc82504fe298ed114857ed96
Safwan Masarik <safwanmasarik@gmail.com>
2023-03-08 18:20:58 +0800

    upgrade mssql package to latest

--- shared/helpers/mssql/index.ts
+++ shared/helpers/mssql/index.ts
@@ -1,7 +1,21 @@
 import sql from "mssql/msnodesqlv8";
 
 export function initialiseConnection() {
-    let config = process.env["SQLConnectionString"];
+    let connectionString = process.env["SQLConnectionString"];
+
+    const config = {
+        driver: "SQL Server",
+        user: process.env["SQLUsername"],
+        password: process.env["SQLPassword"],
+        server: process.env["SQLServer"],
+        database: process.env["SQLDatabase"],
+        options: {
+            encrypt: false, // for azure set to true
+            trustedConnection: true, // Use Windows authentication
+            trustServerCertificate: false // change to true for local dev / self-signed certs
+        }
+    };
+
     return new sql.ConnectionPool(config)
 }
 
